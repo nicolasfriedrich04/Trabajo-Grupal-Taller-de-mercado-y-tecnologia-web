f44f225528c690f0826d129f08f4f2b29d29a2ec
ElvisM2526 <marcaigor003@gmail.com>
2026-06-25 18:23:41 -0300

    refactor: muevo archivo JS a la carpeta botton y agrego modo oscuro

--- Trabajo-Grupal-Taller-de-mercado-y-tecnologia-web-1/estilos.css/Botton/oscuro.js
+++ Trabajo-Grupal-Taller-de-mercado-y-tecnologia-web-1/estilos.css/Botton/oscuro.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const boton = document.getElementById("btn-modo");
 
     boton.addEventListener("click", () => {
-        // Alterna la clase 'oscuro' en el body
+        /* Alterna la clase 'oscuro' en el body */
         document.body.classList.toggle("oscuro");
         
-        // Cambia el texto y el icono del botón
+        /* Cambia el texto y el icono del botón */
         if (document.body.classList.contains("oscuro")) {
             boton.innerText = "☀️ Modo claro";
         } else {
